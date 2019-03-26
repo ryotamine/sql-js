4a397edf699b3d29df9bb92d6afad072144a06d8
Ryota Mine <ryotamine@gmail.com>
2019-03-26 18:22:18 +0000

    Query DB

--- test_script.js
+++ test_script.js
@@ -1,1 +1,27 @@
-// First commit
+// Basic PostgreSQL setup
+const pg = require("pg");
+const settings = require("./settings");
+
+// Connect client information
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+// Check for connection error
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query("SELECT $1::int AS number", ["1"], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(result.rows[0].number);
+    client.end();
+  });
+});
